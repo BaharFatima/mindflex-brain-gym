552818f833bab3f54d3944d10414c2eed63a7b66
Bahar Fatima <baharfatimaa@gmail.com>
2026-02-22 20:23:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Server running"));
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
