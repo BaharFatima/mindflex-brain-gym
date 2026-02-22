8c734111133315ccfd0dfaa77be249d79a2fd835
Bahar Fatima <baharfatimaa@gmail.com>
2026-02-22 20:25:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server running");
+ res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
